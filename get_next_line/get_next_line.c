11c360ff46d6911c18cda98538859283f730443e
Eleonora Guiducci <elguiduc@c1r5p10.42firenze.it>
2025-12-16 12:31:16 +0100

    main

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,11 +6,31 @@
 /*   By: elguiduc <elguiduc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 22:38:25 by elguiduc          #+#    #+#             */
-/*   Updated: 2025/12/15 22:39:18 by elguiduc         ###   ########.fr       */
+/*   Updated: 2025/12/16 11:32:40 by elguiduc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
 char *get_next_line(int fd)
 {
+	size_t	count;
+	void *buf;
+	ssize_t	len;
 	
+	buf = malloc(count + 1);
+	len = read(fd, buf, count);
+	if (!buf )
+		return (NULL);
+	return (buf);
+}
+
+int	main(void)
+{
+	int	fd;
+	char	*str;
+
+	fd = open("file_da_leggere", O_RDONLY);
+	str = get_next_line(fd);
+	return (0);
 }
--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,21 @@
 /*   By: elguiduc <elguiduc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 22:38:14 by elguiduc          #+#    #+#             */
-/*   Updated: 2025/12/15 22:38:15 by elguiduc         ###   ########.fr       */
+/*   Updated: 2025/12/16 11:58:12 by elguiduc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE
+#endif
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#endif

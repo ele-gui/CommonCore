f12ce65f9af127a6c093fab6993c1af363d51d05
Eleonora Guiducci <elguiduc@c1r5p10.42firenze.it>
2025-12-19 18:38:38 +0100

    modifiche e test

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,20 +6,24 @@
 /*   By: elguiduc <elguiduc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 22:38:25 by elguiduc          #+#    #+#             */
-/*   Updated: 2025/12/16 11:32:40 by elguiduc         ###   ########.fr       */
+/*   Updated: 2025/12/19 14:38:28 by elguiduc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	size_t	count;
 	void *buf;
 	ssize_t	len;
 	
-	buf = malloc(count + 1);
-	len = read(fd, buf, count);
+	
+	len = read(fd, buf, len);
+	if (len > BUFFER_SIZE)
+		len = BUFFER_SIZE;
+	buf = ft_calloc(len + 1, sizeof(char *));
+	ft_memcpy(buf, BOHHHHH, len);
 	if (!buf )
 		return (NULL);
 	return (buf);
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -6,7 +6,61 @@
 /*   By: elguiduc <elguiduc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/15 22:38:30 by elguiduc          #+#    #+#             */
-/*   Updated: 2025/12/15 22:38:31 by elguiduc         ###   ########.fr       */
+/*   Updated: 2025/12/19 14:13:04 by elguiduc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	int				i;
+	unsigned char	*temp;
+
+	temp = (unsigned char *)s;
+	i = 0;
+	while (n > 0)
+	{
+		temp[i] = c;
+		n--;
+		i++;
+	}
+	return (s);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	s = ft_memset(s, '\0', n);
+}
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*mem;
+
+	if (nmemb != 0 && size >= __SIZE_MAX__ / nmemb)
+		return (NULL);
+	mem = malloc(nmemb * size);
+	if (!mem)
+		return (NULL);
+	ft_bzero(mem, nmemb * size);
+	return (mem);
+}
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t				i;
+	unsigned char		*d;
+	const unsigned char	*s;
+
+	if (!dest && !src)
+		return (NULL);
+	d = (unsigned char *)dest;
+	s = (const unsigned char *)src;
+	i = 0;
+	while (i < n)
+	{
+		d[i] = s[i];
+		i++;
+	}
+	return (dest);
+}

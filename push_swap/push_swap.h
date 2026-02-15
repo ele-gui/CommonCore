227568906d1dd831594ed0dc986223318e8c4c03
Eleonora <eleonoraguiducci13@gmail.com>
2026-02-15 15:52:32 +0100

    sorting, norm, cost_chunk vuoti

--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: elguiduc <elguiduc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/29 14:36:51 by elguiduc          #+#    #+#             */
-/*   Updated: 2026/02/15 10:10:59 by elguiduc         ###   ########.fr       */
+/*   Updated: 2026/02/15 15:52:01 by elguiduc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,55 +32,35 @@ typedef struct s_chunk
 }	t_chunk;
 
 /* FUNCTIONS */
-int move_to_top_b_and_push_a(t_push_swap *ps)
-;
-int push_chunk_to_b(t_push_swap *ps, t_chunk chunk)
-;
-char	**handle_split(int argc, char **argv)
-;
-int	mini_sort(t_push_swap *ps)
-;
-int	sort_20(t_push_swap *ps)
-;
-int	is_sorted(t_push_swap *ps)
-;
-int sort_general(t_push_swap *ps)
-;
-int sort(t_push_swap *ps)
-;
-int	move_cheapest(t_push_swap *ps, int index)
-;
-int	calculate_cost_min(t_push_swap *ps, int index)
-;
-t_chunk	chunk_division(t_push_swap *ps)
-;
-int	*copy_array(int *arr, int size);
-void	sort_array(int *arr, int size);
-int	find_index(int *sorted, int size, int value);
-void	normalize_stack(t_push_swap *ps);
-int	is_sorted(t_push_swap *ps);
-int	memory_alloc(int argc, t_push_swap *ps);
-long	ft_atol(const char *nptr);
-void	argv_to_stack(int argc, char **argv, t_push_swap *ps);
-int		parse_input(int argc, char **argv);
-void	print_stack(t_push_swap *ps);
-int	max(int a, int b);
-int	min(t_push_swap *ps);
-int		main(int argc, char **argv);
 
-int	sort_3(t_push_swap *ps);
-int	sort_5(t_push_swap *ps);
-int	mini_sort(t_push_swap *ps);
-int	sa(t_push_swap *ps);
-int	sb(t_push_swap *ps);
-int	ss(t_push_swap *ps);
-int	pa(t_push_swap *ps);
-int	pb(t_push_swap *ps);
-int	ra(t_push_swap *ps);
-int	rb(t_push_swap *ps);
-int	rr(t_push_swap *ps);
-int	rra(t_push_swap *ps);
-int	rrb(t_push_swap *ps);
-int	rrr(t_push_swap *ps);
+//mini sort < 20
+int		mini_sort(t_push_swap *ps);
+
+//utils
+int		memory_alloc(int argc, t_push_swap *ps);
+void	argv_to_stack(int argc, char **argv, t_push_swap *ps);
+long	ft_atol(const char *nptr);
+int		min(t_push_swap *ps);
+int		max(int a, int b);
+
+//print stack DA TOGLIERE
+void	print_stack(t_push_swap *ps);
+
+//parsing
+int		parse_input(int argc, char **argv);
+char	**handle_split(int argc, char **argv);
+
+//mosse
+int		sa(t_push_swap *ps);
+int		sb(t_push_swap *ps);
+int		ss(t_push_swap *ps);
+int		pa(t_push_swap *ps);
+int		pb(t_push_swap *ps);
+int		ra(t_push_swap *ps);
+int		rb(t_push_swap *ps);
+int		rr(t_push_swap *ps);
+int		rra(t_push_swap *ps);
+int		rrb(t_push_swap *ps);
+int		rrr(t_push_swap *ps);
 
 #endif
--- push_swap/sorting.c
+++ push_swap/sorting.c
@@ -6,7 +6,7 @@
 /*   By: elguiduc <elguiduc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/08 17:45:54 by elguiduc          #+#    #+#             */
-/*   Updated: 2026/02/15 10:38:08 by elguiduc         ###   ########.fr       */
+/*   Updated: 2026/02/15 15:44:13 by elguiduc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,100 +15,4 @@
 
 #include "push_swap.h"
 
-int	is_sorted(t_push_swap *ps)
-{
-	int i;
-	
-	if (ps->size_a <= 1)
-		return (1);
-	i = 0;
-	while (i < ps->size_a - 1)
-	{
-		if (ps->stack_a[i] > ps->stack_a[i + 1])
-			return (0);
-		i++;
-	}
-	return (1);
-}
 
-int find_max_index(int *stack, int size)
-{
-	int i = 0;
-	int max = stack[0];
-	int index = 0;
-
-	for (i = 1; i < size; i++)
-	{
-		if (stack[i] > max)
-		{
-			max = stack[i];
-			index = i;
-		}
-	}
-	return index;
-}
-
-int move_to_top_b_and_push_a(t_push_swap *ps)
-{
-	int mosse;
-	int	rotations;
-	int	index;
-
-	mosse = 0;
-	// if (index < 0 || index >= ps->size_b)
-	// 	return (0);
-	while (ps->size_b > 0)
-	{
-		index = find_max_index(ps->stack_b, ps->size_b);
-		if (index < ps->size_b / 2)
-		{
-			while (index-- > 0)
-				mosse += rb(ps);
-		}
-		else
-		{
-			rotations = ps->size_b - index;
-			while (rotations-- > 0)
-				mosse += rrb(ps);
-		}
-		mosse += pa(ps);
-		if (ps->size_a > 1 && ps->stack_a[0] > ps->stack_a[1])
-			mosse += sa(ps);
-	}
-	return (mosse);
-}
-
-int sort_general(t_push_swap *ps)
-{
-	t_chunk chunk;
-	int mosse;
-	// int index;
-
-	mosse = 0;
-	chunk = chunk_division(ps);
-	// while (ps->size_a > 0)
-	mosse += push_chunk_to_b(ps, chunk);
-	// while (ps->size_b > 0)
-	// {
-	// 	index = find_max_index(ps->stack_b, ps->size_b);
-	// 	mosse += move_to_top_b_and_push_a(ps, index);
-	// 	// mosse += pa(ps);
-	// }
-	// while (ps->size_b > 0)
-	mosse += move_to_top_b_and_push_a(ps);
-	return (mosse);
-}
-
-int sort(t_push_swap *ps)
-{
-	while (!is_sorted(ps))
-	{
-		if (ps->size_a == 2)
-			return (sa(ps));
-		else if (ps->size_a <= 20)
-			return (sort_20(ps));
-		else
-			return (sort_general(ps));
-	}
-	return (0);
-}

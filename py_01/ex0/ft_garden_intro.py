7787cef860e9af2f73654ac63afd1fb46d99e3d3
Eleonora Guiducci <elguiduc@c1r5p10.42firenze.it>
2026-02-19 15:53:22 +0100

    da finire

--- py_01/ex0/ft_garden_intro.py
+++ py_01/ex0/ft_garden_intro.py
@@ -1,11 +1,16 @@
 #!/urs/bin/env python3
 
+def ft_garden_intro():
+	print("=== Welcome to My Garden ===")
+	plant_name = "Rose"
+	plant_height = 25
+	plant_age = 30
+	print(f"Plant: {plant_name}")
+	print(f"Height: {plant_height}cm")
+	print(f"Age: {plant_age} days")
+	print()
+	print("=== End of Program ===")
+
 if __name__ == "__main__":
-    print("=== Welcome to My Garden ===")
-    plant_name = "Rose"
-    plant_height = 25
-    plant_age = 30
-    print(f"Plant: {plant_name}")
-    print(f"Height: {plant_height}cm")
-    print(f"Age: {plant_age} days")
-    print("=== End of Program ===")
+    ft_garden_intro()
+

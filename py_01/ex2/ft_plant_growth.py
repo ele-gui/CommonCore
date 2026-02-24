c4175ff2a90f8738d0ab5cab4c585507efaf3beb
Eleonora <eleonoraguiducci13@gmail.com>
2026-02-24 12:03:05 +0100

    modificato piante

--- py_01/ex2/ft_plant_growth.py
+++ py_01/ex2/ft_plant_growth.py
@@ -15,7 +15,26 @@ class Plant:
         return (f"{self.name}: {self.height}cm, {self.age} days old")
 
 
+
 def ft_plant_growth():
+    plants = [
+        Plant("Rose", 25, 30),
+        Plant("Sunflower", 80, 45),
+        Plant("Cactus", 15, 120)
+    ]
+    for plant in plants:
+        print("=== Day 1 ===")
+        print(plant.get_info())
+        height_before = plant.height
+        for day in range(6):
+            plant.age_one_day()
+        print("=== Day 7 ===")
+        print(plant.get_info())
+        growth = plant.height - height_before
+        print(f"Growth this week: +{growth}cm")
+        print()
+
+"""def ft_plant_growth():
 	print("=== Day 1 ===")
 	p1 = Plant("Rose", 25, 30)
 	print(p1.get_info())
@@ -28,7 +47,7 @@ def ft_plant_growth():
 	height_after = p1.height
 
 	growth = height_after - height_before
-	print(f"Growth this week: +{growth}cm")
+	print(f"Growth this week: +{growth}cm")"""
 
 if __name__ == "__main__":
 	ft_plant_growth()
--- py_01/ex3/ft_plant_factory.py
+++ py_01/ex3/ft_plant_factory.py
@@ -4,21 +4,23 @@ class Plant:
         self.height = height
         self.age = age
 
-    def    creation(self):
+    def    get_info(self):
         return (f"Created: {self.name} ({self.height}cm, {self.age} days)")
 
 
 def ft_plant_factory():
     print("=== Plant Factory Output ===")
-    p1 = Plant("Rose", 25, 30)
-    p2 = Plant("Oak", 200, 365)
-    p3 = Plant("Cactus", 5, 90)
-    p4 = Plant("Sunflower", 80, 45)
-    p5 = Plant("Fern", 15, 120)
 
-    plants = [p1, p2, p3, p4, p5]
+    plants = [
+        Plant("Rose", 25, 30),
+        Plant("Oak", 200, 365),
+        Plant("Cactus", 5, 120),
+        Plant("Sunflower", 80, 45),
+        Plant("Fern", 15, 120)
+    ]
+
     for i in range(5):
-        print(plants[i].creation())
+        print(plants[i].get_info())
     print()
     print(f"Total plants created: {i + 1}")
 
